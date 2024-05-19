0d72203cfc4abaf145f583a0ab3db66c8e625fe5
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2024-05-19 15:16:47 +0300

    dialog module changes

--- src/components/dialog/dialog.stories.tsx
+++ src/components/dialog/dialog.stories.tsx
@@ -8,8 +8,6 @@ import { Input } from '../input/input';
 const meta = {
   title: 'Modals/Dialog',
   component: Dialog,
-  parameters: {
-  },
   args: {
     hasCloseBtn: true,
     title: 'Heading',
@@ -17,61 +15,23 @@ const meta = {
   render: (args) => {
     const [isOpen, setIsOpen, onKeyDown] = useDialog(true);
     return (
-      <>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et leo risus. Vivamus feugiat est pharetra malesuada pellentesque. Nunc nulla leo, varius quis efficitur in, convallis sit amet sapien. Etiam vel magna elit. Duis porttitor vulputate turpis, vitae auctor metus euismod vitae. In neque leo, ornare sed neque eget, pulvinar auctor sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In vehicula arcu risus, vel sodales neque imperdiet sed. Nunc non dolor vel nibh faucibus fringilla. Mauris orci eros, pellentesque non ornare non, porttitor eget enim. Proin quis facilisis lorem, eget egestas diam.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et leo risus. Vivamus feugiat est pharetra malesuada pellentesque. Nunc nulla leo, varius quis efficitur in, convallis sit amet sapien. Etiam vel magna elit. Duis porttitor vulputate turpis, vitae auctor metus euismod vitae. In neque leo, ornare sed neque eget, pulvinar auctor sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In vehicula arcu risus, vel sodales neque imperdiet sed. Nunc non dolor vel nibh faucibus fringilla. Mauris orci eros, pellentesque non ornare non, porttitor eget enim. Proin quis facilisis lorem, eget egestas diam.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et leo risus. Vivamus feugiat est pharetra malesuada pellentesque. Nunc nulla leo, varius quis efficitur in, convallis sit amet sapien. Etiam vel magna elit. Duis porttitor vulputate turpis, vitae auctor metus euismod vitae. In neque leo, ornare sed neque eget, pulvinar auctor sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In vehicula arcu risus, vel sodales neque imperdiet sed. Nunc non dolor vel nibh faucibus fringilla. Mauris orci eros, pellentesque non ornare non, porttitor eget enim. Proin quis facilisis lorem, eget egestas diam.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et leo risus. Vivamus feugiat est pharetra malesuada pellentesque. Nunc nulla leo, varius quis efficitur in, convallis sit amet sapien. Etiam vel magna elit. Duis porttitor vulputate turpis, vitae auctor metus euismod vitae. In neque leo, ornare sed neque eget, pulvinar auctor sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In vehicula arcu risus, vel sodales neque imperdiet sed. Nunc non dolor vel nibh faucibus fringilla. Mauris orci eros, pellentesque non ornare non, porttitor eget enim. Proin quis facilisis lorem, eget egestas diam.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et leo risus. Vivamus feugiat est pharetra malesuada pellentesque. Nunc nulla leo, varius quis efficitur in, convallis sit amet sapien. Etiam vel magna elit. Duis porttitor vulputate turpis, vitae auctor metus euismod vitae. In neque leo, ornare sed neque eget, pulvinar auctor sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In vehicula arcu risus, vel sodales neque imperdiet sed. Nunc non dolor vel nibh faucibus fringilla. Mauris orci eros, pellentesque non ornare non, porttitor eget enim. Proin quis facilisis lorem, eget egestas diam.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et leo risus. Vivamus feugiat est pharetra malesuada pellentesque. Nunc nulla leo, varius quis efficitur in, convallis sit amet sapien. Etiam vel magna elit. Duis porttitor vulputate turpis, vitae auctor metus euismod vitae. In neque leo, ornare sed neque eget, pulvinar auctor sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In vehicula arcu risus, vel sodales neque imperdiet sed. Nunc non dolor vel nibh faucibus fringilla. Mauris orci eros, pellentesque non ornare non, porttitor eget enim. Proin quis facilisis lorem, eget egestas diam.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et leo risus. Vivamus feugiat est pharetra malesuada pellentesque. Nunc nulla leo, varius quis efficitur in, convallis sit amet sapien. Etiam vel magna elit. Duis porttitor vulputate turpis, vitae auctor metus euismod vitae. In neque leo, ornare sed neque eget, pulvinar auctor sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In vehicula arcu risus, vel sodales neque imperdiet sed. Nunc non dolor vel nibh faucibus fringilla. Mauris orci eros, pellentesque non ornare non, porttitor eget enim. Proin quis facilisis lorem, eget egestas diam.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et leo risus. Vivamus feugiat est pharetra malesuada pellentesque. Nunc nulla leo, varius quis efficitur in, convallis sit amet sapien. Etiam vel magna elit. Duis porttitor vulputate turpis, vitae auctor metus euismod vitae. In neque leo, ornare sed neque eget, pulvinar auctor sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In vehicula arcu risus, vel sodales neque imperdiet sed. Nunc non dolor vel nibh faucibus fringilla. Mauris orci eros, pellentesque non ornare non, porttitor eget enim. Proin quis facilisis lorem, eget egestas diam.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et leo risus. Vivamus feugiat est pharetra malesuada pellentesque. Nunc nulla leo, varius quis efficitur in, convallis sit amet sapien. Etiam vel magna elit. Duis porttitor vulputate turpis, vitae auctor metus euismod vitae. In neque leo, ornare sed neque eget, pulvinar auctor sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In vehicula arcu risus, vel sodales neque imperdiet sed. Nunc non dolor vel nibh faucibus fringilla. Mauris orci eros, pellentesque non ornare non, porttitor eget enim. Proin quis facilisis lorem, eget egestas diam.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et leo risus. Vivamus feugiat est pharetra malesuada pellentesque. Nunc nulla leo, varius quis efficitur in, convallis sit amet sapien. Etiam vel magna elit. Duis porttitor vulputate turpis, vitae auctor metus euismod vitae. In neque leo, ornare sed neque eget, pulvinar auctor sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In vehicula arcu risus, vel sodales neque imperdiet sed. Nunc non dolor vel nibh faucibus fringilla. Mauris orci eros, pellentesque non ornare non, porttitor eget enim. Proin quis facilisis lorem, eget egestas diam.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et leo risus. Vivamus feugiat est pharetra malesuada pellentesque. Nunc nulla leo, varius quis efficitur in, convallis sit amet sapien. Etiam vel magna elit. Duis porttitor vulputate turpis, vitae auctor metus euismod vitae. In neque leo, ornare sed neque eget, pulvinar auctor sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In vehicula arcu risus, vel sodales neque imperdiet sed. Nunc non dolor vel nibh faucibus fringilla. Mauris orci eros, pellentesque non ornare non, porttitor eget enim. Proin quis facilisis lorem, eget egestas diam.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et leo risus. Vivamus feugiat est pharetra malesuada pellentesque. Nunc nulla leo, varius quis efficitur in, convallis sit amet sapien. Etiam vel magna elit. Duis porttitor vulputate turpis, vitae auctor metus euismod vitae. In neque leo, ornare sed neque eget, pulvinar auctor sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In vehicula arcu risus, vel sodales neque imperdiet sed. Nunc non dolor vel nibh faucibus fringilla. Mauris orci eros, pellentesque non ornare non, porttitor eget enim. Proin quis facilisis lorem, eget egestas diam.
-        </p>
-        <Dialog
-          isOpen={isOpen}
-          hasCloseBtn={args.hasCloseBtn}
-          onClose={() => setIsOpen(false)}
-          onKeyDown={onKeyDown}
-          title={args.title}
-        >
-          <form method="dialog">
-            <DialogForm>
-              <Input required={true} type="text" placeholder="Title" />
-              <Input required={true} type="number" placeholder="Amount" />
-            </DialogForm>
-            <DialogFooter>
-              <Button autoFocus={true}>Save Changes</Button>
-            </DialogFooter>
-          </form>
-        </Dialog>
-      </>
+      <Dialog
+        isOpen={isOpen}
+        hasCloseBtn={args.hasCloseBtn}
+        onClose={() => setIsOpen(false)}
+        onKeyDown={onKeyDown}
+        title={args.title}
+      >
+        <form method="dialog">
+          <DialogForm>
+            <Input required={true} type="text" placeholder="Title" />
+            <Input required={true} type="number" placeholder="Amount" />
+          </DialogForm>
+          <DialogFooter>
+            <Button autoFocus={true}>Save Changes</Button>
+          </DialogFooter>
+        </form>
+      </Dialog>
     );
   }
 } satisfies Meta<typeof Dialog>;
