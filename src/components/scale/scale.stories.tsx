0ec9fbdc2215c0a2b2519359ce4e802631f1cd81
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2024-08-17 08:32:01 +0300

    wip scale

---
+++ src/components/scale/scale.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Scale } from './scale';
+
+const meta = {
+  title: 'Data Visualization/Scale',
+  component: Scale,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Scale>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    topValue: 5000,
+    topLabel: 'Planned',
+    unit: '$',
+    progress: 110,
+    leftValue: 3000,
+    leftLabel: 'Actual',
+    rightValue: 2000,
+    rightLabel: 'Current Cash'
+  }
+};
---
+++ src/components/scale/scale.tsx
@@ -0,0 +1,89 @@
+import { clsx } from 'clsx';
+import styles from './scale.module.sass';
+
+type ScaleProps = Readonly<{
+  unit: string,
+  topValue: number,
+  topLabel: string,
+  leftValue: number,
+  leftLabel: string,
+  rightValue: number,
+  rightLabel: string,
+  progress: number,
+}>;
+
+export const Scale = (props: ScaleProps) => (
+  <div className={
+    clsx(
+      styles['scale'],
+      props.progress > 100 && styles['scale--overflow']
+    )
+  }>
+    <div className={styles['scale__topLabel']}>
+      <div className={styles['scale__textNode']}>
+        <div className={styles['scale__textNode__value']}>
+          {props.topValue}{props.unit}
+        </div>
+        <div className={styles['scale__textNode__label']}>
+          {props.topLabel}
+        </div>
+      </div>
+    </div>
+    
+    <div className={styles['scale__progress__wrap']}>
+      <div
+        className={styles['scale__progress']}
+      >
+        <progress
+          max="100"
+          value={props.progress}
+          className={styles['scale__a11y']}
+        />
+
+      
+        <div
+          style={{
+            width: `calc(${props.progress}% - 0.179rem`
+          }}
+          className={styles['scale__progress__bar']}
+        >
+          <div className={styles['scale__progress__bar__label']}>
+            <div className={
+              clsx(
+                styles['scale__textNode'],
+                styles['scale__textNode--progress']
+              )
+            }>
+              <div className={styles['scale__textNode__value']}>
+                {props.leftValue}{props.unit}
+              </div>
+              <div className={styles['scale__textNode__label']}>
+                {props.leftLabel}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div
+        className={styles['scale__progress__balance']}
+      >
+        <div className={styles['scale__progress__bar__label']}>
+          <div className={
+            clsx(
+              styles['scale__textNode'],
+              styles['scale__textNode--balance']
+            )
+          }>
+            <div className={styles['scale__textNode__value']}>
+              {props.rightValue}{props.unit}
+            </div>
+            <div className={styles['scale__textNode__label']}>
+              {props.rightLabel}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
