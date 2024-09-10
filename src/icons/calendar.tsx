b4f18a06686c8894d7545bcdebacdfea018963ca
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2024-09-10 11:12:07 +0300

    update icons

--- src/icons/calendar.tsx
+++ src/icons/calendar.tsx
@@ -3,7 +3,7 @@ import { IconProps } from '../components/icon/icon.types';
 export const CalendarIcon = (props: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 48 54"
+    viewBox="0 0 24 24"
     width={props.width}
     height={props.height}
     fill={props.fill}
@@ -11,6 +11,6 @@ export const CalendarIcon = (props: IconProps) => (
     className={props.className}
   >
     <title lang="en">calendar icon</title>
-    <path d="M16.2857 2.53125C16.2857 1.12852 15.1393 0 13.7143 0C12.2893 0 11.1429 1.12852 11.1429 2.53125V6.75H6.85714C3.075 6.75 0 9.77695 0 13.5V15.1875V20.25V47.25C0 50.973 3.075 54 6.85714 54H41.1429C44.925 54 48 50.973 48 47.25V20.25V15.1875V13.5C48 9.77695 44.925 6.75 41.1429 6.75H36.8571V2.53125C36.8571 1.12852 35.7107 0 34.2857 0C32.8607 0 31.7143 1.12852 31.7143 2.53125V6.75H16.2857V2.53125ZM5.14286 20.25H42.8571V47.25C42.8571 48.1781 42.0857 48.9375 41.1429 48.9375H6.85714C5.91429 48.9375 5.14286 48.1781 5.14286 47.25V20.25Z" />
+    <path d="M3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-2V2h-2v2H9V2H7v2H5a2 2 0 0 0-2 2zm16 14H5V8h14z"/>
   </svg>
 );
--- src/icons/logout.tsx
+++ src/icons/logout.tsx
@@ -3,7 +3,7 @@ import { IconProps } from '../components/icon/icon.types';
 export const LogoutIcon = (props: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 512 512"
+    viewBox="0 0 24 24"
     width={props.width}
     height={props.height}
     fill={props.fill}
@@ -11,6 +11,7 @@ export const LogoutIcon = (props: IconProps) => (
     className={props.className}
   >
     <title lang="en">logout icon</title>
-    <path d="m9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0 12.5 12.5 12.5 32.8 0 45.3l-73.4 73.4h210.7c17.7 0 32 14.3 32 32 0 17.7-14.3 32-32 32h-210.7l73.4 73.4c12.5 12.5 12.5 32.8 0 45.3-12.5 12.5-32.8 12.5-45.3 0l-128-128zm342.6-182.6c-17.7 0-32-14.3-32-32 0-17.7 14.3-32 32-32h64c53 0 96 43 96 96v256c0 53-43 96-96 96h-64c-17.7 0-32-14.3-32-32 0-17.7 14.3-32 32-32h64c17.7 0 32-14.3 32-32v-256c0-17.7-14.3-32-32-32z"/>
+    <path d="M16 13v-2H7V8l-5 4 5 4v-3z"/>
+    <path d="M20 3h-9c-1.103 0-2 .897-2 2v4h2V5h9v14h-9v-4H9v4c0 1.103.897 2 2 2h9c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2z"/>
   </svg>
 );
