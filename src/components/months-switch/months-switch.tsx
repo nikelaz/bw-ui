f3731e870544b17c37d902998ece897d3f1e7fee
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2024-09-10 11:28:16 +0300

    update themes

--- src/components/months-switch/months-switch.tsx
+++ src/components/months-switch/months-switch.tsx
@@ -7,8 +7,8 @@ type MonthsSwitchProps = Readonly<{
 
 export const MonthsSwitch = (props: MonthsSwitchProps) => (
   <span className={styles['months-switch']}>
-    <Icon type={IconTypes.Calendar} className={styles['months-switch__left-icon']} fill="#555" />
-    {props.children}
+    <Icon type={IconTypes.Calendar} className={styles['months-switch__left-icon']} fill="currentColor" />
+    <span className={styles['months-switch__content']}>{props.children}</span>
     <Icon type={IconTypes.ChevronDown} className={styles['months-switch__chevron-icon']}/>
   </span>
 );
